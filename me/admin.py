b670af4abfe01b45c2209a82a8df42a0b626ad09
kennedy-ak <akogokennedy@gmail.com>
2023-12-15 02:18:03 +0000

    adding the admin view

--- me/admin.py
+++ me/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Policeprofile,CitizenProfile, Complains
 # Register your models here.
-admin.site.register(Policeprofile)
-admin.site.register(CitizenProfile)
 
-admin.site.register(Complains)
+@admin.register(Policeprofile)
+
+class PoliceprofileAdmin(admin.ModelAdmin):
+    list_display = ('service_id','first_name','last_name','rank')
+    search_fields = ('service_id',)
+    list_filter = ('rank',)
+
+
+
+@admin.register(CitizenProfile)
+class CitzenAdmin(admin.ModelAdmin):
+    list_display =('username','first_name','last_name','drivers_license_id','ghana_card_id','phone_number')
+    search_fields = ('drivers_license_id','ghana_card_id')
+    
+
+@admin.register(Complains)
+
+class ComplainsAdmin(admin.ModelAdmin):
+    list_display = ('officer','citizens','date_created','region')
+    search_fields = ('officer','citizens')
+    list_filter =('citizens','officer','date_created')
