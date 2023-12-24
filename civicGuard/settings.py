aac401ea9cbcfd898eafb6949af115782a934a36
kennedy-ak <akogokennedy@gmail.com>
2023-12-24 17:33:19 +0000

    using postgress

--- civicGuard/settings.py
+++ civicGuard/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'civicGuard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'civicguard',
+        'USER': 'civicguard',
+        'PASSWORD': 'akogo1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
